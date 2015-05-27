a019542f7e09489d3a74e0c8b885352a614f8438
Administrador <Administrador@SPO_LAB0318.inacap.cl>
2015-05-27 10:06:21 -0300

    añadida clase CONEXION

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -5,10 +5,47 @@
  */
 package Clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Administrador
+ * @author heltonsmith
  */
 public class Conexion {
     
+    public Connection con;
+
+    public Conexion() {
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //Driver class Conexion
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/farmaciasandlud?zeroDateTimeBehavior=convertToNull", "root", ""); //URL de Conexion
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public void cerrar(){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    
+    
+            
 }
