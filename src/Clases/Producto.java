579bca2f6fb8c0c825f54ff84c67e5dfe03e7909
DavidDeFilippi <d.defilippi@asistenciapcmaipu.cl>
2015-06-03 09:33:01 -0300

    Se actualiza clase Producto.java

--- src/Clases/Producto.java
+++ src/Clases/Producto.java
@@ -10,5 +10,70 @@ package Clases;
  * @author David
  */
 public class Producto {
+    private String codigo;
+    private String marca;
+    private String componente;
+    private String categoria;
+    private int precio;
+    private int stock;
+
+    public Producto(String codigo, String marca, String componente, String categoria, int precio, int stock) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.componente = componente;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getComponente() {
+        return componente;
+    }
+
+    public void setComponente(String componente) {
+        this.componente = componente;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    
     
 }
